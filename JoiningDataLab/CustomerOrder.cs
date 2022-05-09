199aa25675797e40fa22b8fca3f689d6c77469ed
Aaliyah Kayum <kayumaaliyah@gmail.com>
2022-05-09 09:12:58 -0400

    chicken

---
+++ JoiningDataLab/CustomerOrder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoiningDataLab
+{
+    class Order
+    {
+        public string CustomerName { get; set; }
+        public string Item { get; set; }
+        public Decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Decimal Total { get; set; }
+
+
+        public Order(string customerName, string item, decimal price, int quantity, decimal total)
+        {
+            this.CustomerName = customerName;
+            this.Item = item;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Total = total;
+        }
+        public void orderTotal(int Quantity, decimal total)
+        {
+            total = 2 * Quantity;
+        }
+        public void Header(Order selectOrder)
+        {
+            Console.WriteLine($"{selectOrder}");
+            Console.WriteLine();
+            Console.WriteLine("Customer\tItem\tPrice\tQuantity\tTotal");
+            Console.WriteLine($"{selectOrder.CustomerName}\t{selectOrder.Item}\t{selectOrder.Price}\t{selectOrder.Quantity}\t{selectOrder.Total}");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ JoiningDataLab/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace JoiningDataLab
+{
+    public class Program
+    { 
+        public static void Main()
+        {
+            List<Order> ordersPlaced = new List<Order>();
+
+            Order one = new Order("Acme Hardware", "Mouse", 25, 3, 75);
+            ordersPlaced.Add(one);
+            Order two = new Order("Acme Hardware", "Keyboard", 45, 2, 90);
+            ordersPlaced.Add(two);
+            Order three = new Order("Falls Realty", "Macbook", 800, 2, 1600);
+            ordersPlaced.Add(@three);
+            Order four = new Order("Julie's Morning Diner", "iPad", 525, 1, 525);
+            ordersPlaced.Add(@four);
+            Order five = new Order("Julie's Morning Diner", "Credit Card Reader", 45, 1, 45);
+            ordersPlaced.Add(five);
+            ordersPlaced.ToArray();
+
+            bool runAgain = true;
+            while (runAgain)
+            {
+                foreach (Order order in ordersPlaced)
+                {
+                    //order.//(ordersPlaced);
+                    order.Header(order);
+                }
+                runAgain = false;
+            }
+
+        }
+    }
+}
